42737af4a77d6c1a91cd8f6ab7f1fc430f794094
pegaser <sperezgabriel@gmail.com>
2026-01-11 16:14:36 -0600

    Agregar archivos de proyecto.

---
+++ CSharp.News.FieldContextual/CSharp.News.FieldContextual/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp.News.FieldContextual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.Age = 25;   // correcto
+            person.Age = -3;   // muestra mensaje y no asigna
+            Console.ReadKey();
+        }
+    }
+
+    internal class Person
+    {
+        public int Age
+        {
+            get => field;
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("La edad no puede ser negativa");
+                    return;
+                }
+
+                field = value;
+            }
+        }
+
+    }
+}
---
+++ ConsoleAppSolution/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "   ";
+            if (texto.IsEmptyIgnoringSpaces)
+            {
+                Console.WriteLine("El texto está vacío");
+            }
+            Console.ReadKey();
+        }
+    }
+    public static class StringExtensions
+    {
+        extension(string text)
+        {
+            public bool IsEmptyIgnoringSpaces
+            {
+                get => string.IsNullOrWhiteSpace(text);
+            }
+        }
+    }
+
+
+}
