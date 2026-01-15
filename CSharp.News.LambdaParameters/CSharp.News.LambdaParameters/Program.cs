b10de17a3fcfa65a786de5306ec6383bc18f6a88
pegaser <sperezgabriel@gmail.com>
2026-01-14 21:48:15 -0600

    CSharp News 14012026

---
+++ CSharp.News.LambdaParameters/CSharp.News.LambdaParameters/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CSharp.News.LambdaParameters
+{
+    internal class Program
+    {
+        delegate int RefOperation(ref int value);
+        static void Main(string[] args)
+        {
+            RefOperation multiplyByTwoRef = (ref int value) => value * 2;
+
+            int number = 5;
+            int result = multiplyByTwoRef(ref number);
+
+            Console.WriteLine("Resultado: " + result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp.News.NameOf/CSharp.News.NameOf/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharp.News.NameOf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age2 = 25;
+
+            Console.WriteLine($"Mi variable se llama {nameof(age2)}");
+
+            var repository = new Repository<int>();
+            repository.ShowTypeName();
+
+            Console.ReadKey();
+        }
+    }
+    public class Repository<T>
+    {
+        public void ShowTypeName()
+        {
+            Console.WriteLine($"{nameof(Repository<>)}");
+
+            throw new Exception($"Error en el repositorio {nameof(Repository<>)}");
+        }
+    }
+
+}
