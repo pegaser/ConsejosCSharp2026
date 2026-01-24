d7b70edc20a27ad89af30125bd1636738e9cb85b
pegaser <sperezgabriel@gmail.com>
2026-01-23 22:55:52 -0600

    CSharp News 23012026

---
+++ CSharp.News.ImplicitSpanConversions/CSharp.News.ImplicitSpanConversions/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp.News.ImplicitSpanConversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Span<int> middle = numbers.AsSpan(1, 3);
+
+
+            Console.WriteLine("Primeros tres números:");
+            foreach (var n in middle)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
